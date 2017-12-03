e8330a1c4551ff6b41b0f4f84591b2c6531f1bc8
robertschaap <robert.schaap@mac.com>
2017-12-03 13:48:47 +0100

    Four spaces for legibility, clarified route comments

--- app.js
+++ app.js
@@ -25,14 +25,14 @@ const sequelize = new Sequelize('reservation', process.env.POSTGRES_USER, proces
 //SESSIONS
 app.use(session({
     store: new SequelizeStore({
-      db: sequelize,
-      checkExpirationInterval: 30 * 60 * 1000,
-      expiration: 24 * 60 * 60 * 1000,
+        db: sequelize,
+        checkExpirationInterval: 30 * 60 * 1000,
+        expiration: 24 * 60 * 60 * 1000,
     }),
     secret: "whatever secret for user",
     saveUninitialized: true,
     resave: false
-  })
+    })
 );
 
 //MODEL CONFIGURATION
@@ -71,193 +71,166 @@ Rooms.belongsToMany(Users, { through: { model: Bookings, unique: false}, foreign
 //syncing database and manually inserting in Postgress
 sequelize.sync({ force: true })
 .then(() => {
-  Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
-  Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
-  Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
-  Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
+    Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
+    Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
+    Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
+    Rooms.create({ roomType: "Standard", roomRate: "250", description: "bla" })
 })
 .then(() => {
-  bcrypt.hash('p', 10).then((hash) => {
-    Users.create({ firstname: 'p', lastname: 'p', email: 'p', phone: 'p', address: 'p', passport: 'p', creditcard: 'p', password: hash })
-  }).then(() => {
-    Bookings.create({ userId:1, roomId:1, dateCheckin: "2017-12-01", dateCheckout:"2017-12-03 ", roomType:"Standard" })
-    Bookings.create({ userId:1, roomId:2, dateCheckin: "2017-12-01", dateCheckout:"2017-12-03 ", roomType:"Standard" })
-    Bookings.create({ userId:1, roomId:3, dateCheckin: "2017-12-01", dateCheckout:"2017-12-03 ", roomType: "Standard" })
-    Bookings.create({ userId:1, roomId:3, dateCheckin: "2017-12-04", dateCheckout:"2017-12-06 ", roomType:"Standard" })
-  })
-  bcrypt.hash('q', 10).then((hash) => {
-    Users.create({ firstname: 'q', lastname: 'q', email: 'q', phone: 'q', address: 'q', passport: 'q', creditcard: 'q', password: hash })
-  })
+    bcrypt.hash('p', 10).then((hash) => {
+        Users.create({ firstname: 'p', lastname: 'p', email: 'p', phone: 'p', address: 'p', passport: 'p', creditcard: 'p', password: hash })
+    }).then(() => {
+        Bookings.create({ userId:1, roomId:1, dateCheckin: "2017-12-01", dateCheckout:"2017-12-03 ", roomType:"Standard" })
+        Bookings.create({ userId:1, roomId:2, dateCheckin: "2017-12-01", dateCheckout:"2017-12-03 ", roomType:"Standard" })
+        Bookings.create({ userId:1, roomId:3, dateCheckin: "2017-12-01", dateCheckout:"2017-12-03 ", roomType: "Standard" })
+        Bookings.create({ userId:1, roomId:3, dateCheckin: "2017-12-04", dateCheckout:"2017-12-06 ", roomType:"Standard" })
+    })
+    bcrypt.hash('q', 10).then((hash) => {
+        Users.create({ firstname: 'q', lastname: 'q', email: 'q', phone: 'q', address: 'q', passport: 'q', creditcard: 'q', password: hash })
+    })
 })
 
 //INDEX/HOME ROUTE
 app.get('/', (req, res) => {
-  res.render('index', {
-    message: req.query.message,
-    user: req.session.user
-  });
+    res.render('index', {
+        message: req.query.message,
+        user: req.session.user
+    });
 });
 
 //USERS LOGIN PAGE IS ON INDEX PAGE
 app.post('/login', (req, res) => {
-  if (req.body.email.length === 0) {
-    res.redirect('/?message=' + encodeURIComponent("Please fill in your username."))
-    return;
-  }
-  if (req.body.password.length === 0) {
-    res.redirect('/?message=' + encodeURIComponent("Please fill in your password."))
-    return;
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-
-  Users.findOne({
-    where: {
-      email: req.body.email
+    if (req.body.email.length === 0) {
+        res.redirect('/?message=' + encodeURIComponent("Please fill in your username."))
+        return;
     }
-  })
-  .then((user) => {
-    const hash = user.password;
-    bcrypt.compare(password,hash)
-    .then((result) => {
-      if (user !== null && password === user.password) {
-        res.redirect(`/profile`);
-      } else {
-      //if incorrect showing error to user
-      res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
-      }
+
+    if (req.body.password.length === 0) {
+        res.redirect('/?message=' + encodeURIComponent("Please fill in your password."))
+    return;
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    Users.findOne({
+        where: {
+            email: req.body.email
+        }
     })
-    .catch((error) => {
-      //if any error occurs showing an invalid message to user
-      console.error(error);
-      res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+    .then((user) => {
+        const hash = user.password;
+        bcrypt.compare(password,hash)
+        .then((result) => {
+            if (user !== null && password === user.password) {
+                res.redirect(`/profile`);
+            } else {
+                //if incorrect showing error to user
+                res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+            }
+        })
+        .catch((error) => {
+            //if any error occurs showing an invalid message to user
+            console.error(error);
+            res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+        })
     })
-  })
 });
 
 //TO REGISTER ROUTE CREATING NEW USER IN DATABASE and starting session for the user and sending them to their profile
 app.get('/register', (req,res) => {
-  res.render('register');
+    res.render('register');
 })
 
 //creating new user in database and starting session for the user and sending them to their profile
 app.post('/register', (req,res) => {
-  if ( req.body.password ) {
-    const password = req.body.password;
-    bcrypt.hash(password, 10)
-    .then((hash) => {
-      User.create({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        phone: req.body.phone,
-        address: req.body.address,
-        passport: req.body.passport,
-        creditcard: req.body.creditcard,
-        password: hash
-      })
-      .then((user) => {
-        req.session.user = user;
-        return
-      })
-      .then(() => {
-        res.redirect('/profile')
-      })
-    })
-    .catch((error) => {
-      console.error(error);
-      res.redirect('/?message=' + encodeURIComponent('Error has occurred. Please check the server.'));
-    })
-  } else {
-    res.render('register', { message: "The passwords don't match!" })
-  };
+    if ( req.body.password ) {
+        const password = req.body.password;
+        bcrypt.hash(password, 10)
+        .then((hash) => {
+            User.create({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                phone: req.body.phone,
+                address: req.body.address,
+                passport: req.body.passport,
+                creditcard: req.body.creditcard,
+                password: hash
+            })
+            .then((user) => {
+                req.session.user = user;
+                return
+            })
+            .then(() => {
+                res.redirect('/profile');
+            })
+        })
+        .catch((error) => {
+            console.error(error);
+            res.redirect('/?message=' + encodeURIComponent('Error has occurred. Please check the server.'));
+        })
+    } else {
+        res.render('register', { message: "The passwords don't match!" });
+    };
 });
 
 app.get('/profile', (req,res) => {
-  const user = req.session.user;
-  if (user === undefined) {
-    res.redirect('/?message=' + encodeURIComponent("Please log in"));
-  } else {
-    res.render('profile', {
-      user: user
-    })
-  }
+    const user = req.session.user;
+        if (user === undefined) {
+        res.redirect('/?message=' + encodeURIComponent("Please log in"));
+    } else {
+        res.render('profile', { user: user });
+    }
 });
 
 //ROUTE TO CHECK AVAILABILITY ROUTE
-//User will make will check availabilty
+//Display availability results from post route
 app.get('/availability', (req,res) => {
-  res.render('availability');
+    res.render('availability');
 })
 
-//creating new reservation as per avaialability in database and starting session for the user and sending them to their profile
+//Search for availability and redirect to availability overview page
 app.post('/availability', (req,res) => {
-  const user = req.session.user;
-  let arrivaldate = req.body.arrivaldate
-  let departuredate = req.body.departuredate
+    const user = req.session.user;
+    let arrivaldate = req.body.arrivaldate
+    let departuredate = req.body.departuredate
 
-  Rooms.findAll()
-  .then((result) => {
-    console.log(result)
-    res.render('availability', {query: result});
-  })
+    Rooms.findAll()
+    .then((result) => {
+        console.log(result)
+        res.render('availability', {query: result});
+    })
 });
 
-//GET BOOKINGS ROUTE CREATING NEW USER IN DATABASE and starting session for the user and sending them to their profile
-app.get('/bookings', (req,res) => {
-  res.render('bookings');
-});
-
-//creating new reservation in database and starting session for the user and sending them to their profile
+//Select a room and send to confirmation page where user checks and confirms booking to be made
 app.post('/bookings', (req,res) => {
-   Booking.create({
-    dateCheckin: req.body.dateCheckin,
-    dateCheckout: req.body.dateCheckout,
-    roomType: req.body.roomType,
-    roomNumber: req.body.roomNumber
-   })
-  .then((Booking) => {
-    res.render('bookings');
-  })
+    Booking.create({
+        dateCheckin: req.body.dateCheckin,
+        dateCheckout: req.body.dateCheckout,
+        roomType: req.body.roomType
+    })
+    .then((Booking) => {
+        res.render('bookings');
+    })
 });
 
 //ROUTE TO CONFIRMATION
-//User will receive a confirmation on what has been booked
+//User will receive a confirmation on booking to be made, checks and confirms if ok
 app.get('/confirmation', (req,res) => {
-  res.render('confirmation');
-})
+    res.render('confirmation');
+});
 
-//creating confirmation in database and starting session for the user and sending them to their profile
+//Creating actual booking in database
 app.post('/confirmation', (req,res) => {
-  res.redirect('/bookings');
-})
-
-//TO BOOKINGS ROUTE CREATING NEW USER IN DATABASE and starting session for the user and sending them to their profile
-app.get('/bookings', (req,res) => {
-  res.render('bookings');
-})
-
-//creating new reservation in database and starting session for the user and sending them to their profile
-app.post('/bookings', (req,res) => {
-  User.create({
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    email: req.body.email,
-    phone: req.body.phone,
-    dateBooked: req.body.dateBooked,
-    dateCheckin: req.body.dateCheckin,
-    dateCheckout: req.body.dateCheckout,
-    roomType: req.body.roomType,
-    roomPreference: req.body.roomPreference,
-    bedPreference: req.body.bedPreference,
-    roomRate: req.body.roomRate
-  })
-  .then(()=> {
-    res.redirect('/confirmation');
-  })
-  .catch(err => {
-    console.error(err)
-  })
+    Booking.create({
+        dateCheckin: req.body.dateCheckin,
+        dateCheckout: req.body.dateCheckout,
+        roomType: req.body.roomType
+    })
+    .then((Booking) => {
+        res.render('confirmed');
+    })
 });
 
 app.listen(myport, () =>
